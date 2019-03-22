2d273125cf2f81250b964bc7ec40b813e1f68a2d
liuyanguu <lyhello@gmail.com>
2019-03-22 13:22:09 -0400

    revise stack plot

--- Blogdown.R
+++ Blogdown.R
@@ -1,10 +1,10 @@
 # some quick commend to use 
 
-getwd()
 library(blogdown)
-serve_site()
-build_site()
+
 
 
 # blogdown::new_site(theme = 'alanorth/hugo-theme-bootstrap4-blog')
 setwd("D:/liuyanguu/Blogdown/hugo-xmag/")
+serve_site()
+build_site()
