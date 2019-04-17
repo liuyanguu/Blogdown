fdcccee04d6313a549aff0fe1075fae099a6a2b6
liuyanguu <lyhello@gmail.com>
2019-04-17 16:08:25 -0400

    add post on map

--- Blogdown.R
+++ Blogdown.R
@@ -1,10 +1,8 @@
 # some quick commend to use 
 
 library(blogdown)
-
-
-
 # blogdown::new_site(theme = 'alanorth/hugo-theme-bootstrap4-blog')
 setwd("D:/liuyanguu/Blogdown/hugo-xmag/")
+
 # serve_site()
 build_site()
