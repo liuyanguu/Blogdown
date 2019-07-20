9ae4ca91920e8b8eeee87897b443893001d30163
liuyanguu <lyhello@gmail.com>
2019-07-20 19:23:53 -0400

    revise links

--- hugo-xmag/Blogdown.R
+++ hugo-xmag/Blogdown.R
@@ -4,9 +4,8 @@ library(blogdown)
 library(here)
 here()
 # blogdown::new_site(theme = 'alanorth/hugo-theme-bootstrap4-blog')
-setwd(here("hugo-xmag"))
+# setwd(here("hugo-xmag"))
 
 serve_site()
-
 
 build_site()
