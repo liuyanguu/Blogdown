5aa1443030adbe911f12141626a3ec76e643702e
liuyanguu <lyhello@gmail.com>
2019-09-16 12:16:36 +0800

    revised

--- hugo-xmag/Blogdown.R
+++ hugo-xmag/Blogdown.R
@@ -2,10 +2,10 @@
 
 library(blogdown)
 library(here)
-here()
+# here()
 # blogdown::new_site(theme = 'alanorth/hugo-theme-bootstrap4-blog')
 # setwd(here("hugo-xmag"))
 
-serve_site()
+# serve_site()
 
 build_site()
