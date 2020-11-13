49939c3d09973dd32b197ba70c2095e52694bc68
liuyanguu <lyhello@gmail.com>
2020-11-13 11:19:44 -0500

    slight revise settings

---
+++ Blogdown.R
@@ -0,0 +1,29 @@
+# some quick commend to use 
+
+library(blogdown)
+library(here)
+
+
+list.of.packages <- c("ggplot2", "data.table","plyr","QuantPsyc",
+                      "glmnet","leaps","randomForest","gbm","caret","xgboost","Ckmeans.1d.dp",
+                      "DiagrammeR", "knitr", "here",
+                      "curl", "gplots", "dendextend", "colorspace",
+                      "raster", "rgdal", "Hmisc", "plyr", "RColorBrewer", "googledrive",
+                      "SHAPforxgboost",
+                      "leafletCN",
+                      "devtools", "drake", "reshape2", "scatterplot3d", "readr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+
+
+# here()
+# blogdown::new_site(theme = 'alanorth/hugo-theme-bootstrap4-blog')
+# setwd(here("hugo-xmag"))
+
+blogdown::stop_server()
+blogdown::serve_site()
+
+# build_site()
+
+
